b38382fc226ee84a3671b5a22eb36da5d3392295
Anton Kudris <akudris@rutube.ru>
2026-04-14 21:08:27 +0300

    cleanup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,20 +4,13 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let mut builder = tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_clipboard_manager::init())
-        .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_os::init())
-        .plugin(tauri_plugin_http::init())
-        .plugin(tauri_plugin_notification::init());
+        .plugin(tauri_plugin_os::init());
 
     #[cfg(desktop)]
     {
-        builder = builder
-            .plugin(tauri_plugin_window_state::Builder::default().build())
-            .plugin(tauri_plugin_global_shortcut::Builder::new().build());
+        builder = builder.plugin(tauri_plugin_window_state::Builder::default().build());
     }
 
     builder
